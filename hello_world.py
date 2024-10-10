e979e4612d6d6b1bcfee541e331a4283d2db9f7d
Patrick <patrickmarkhambrown@gmail.com>
2024-10-10 21:52:06 +1100

    fucking around with new python plugins and seeing if they work in nvim

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,12 @@
-#first file created for crash course
-#\n creates new lin
+# first file created for crash course
+# \n creates new lin
 var0 = 2
 var1 = 4
 print("\nHello, world!\n")
-chloe_response=input("\nHow are you?\n")
-#lower() function makes the comparison case-insensitive
+chloe_response = input("\nHow are you?\n")
+# lower() function makes the comparison case-insensitive
 if chloe_response.lower() == "good" or "greatgreat":
     print("nice!" + str(var0))
 else:
     print("not nice")
+print("this is a test ffs")
